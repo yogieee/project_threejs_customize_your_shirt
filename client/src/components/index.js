7878bc1e25cc1cd7a3d4b7ab5ab4fa8f8fdab670
Yogieee <38261912+yogieee@users.noreply.github.com>
2023-11-09 23:53:50 +0000

    Update index.js

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,4 @@
-import CustomButton from "./customButton";
+import CustomButton from "./CustomButton";
 import AIPicker from "./AIPicker";
 import FilePicker from "./FilePicker";
 import Tab from "./Tab";
